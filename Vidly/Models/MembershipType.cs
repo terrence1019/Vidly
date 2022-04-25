b5b650b26cc5bd93a9f26f6d4fabb2adb858511b
Terrence John <terrence1019@outlook.com>
2022-04-25 19:24:25 -0400

    • Add Display Annotations for the fields of the MembershipType class

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Required for Data Annotations
+using System.ComponentModel.DataAnnotations;
+
+
 namespace Vidly.Models
 {
     public class MembershipType
     {
+        [Display(Name = "Membership Type ID")]
         public byte MembershipTypeID { get; set; }
 
+        [Display(Name = "Membership Type Name")]
         public string MembershipName { get; set; }
 
+        [Display(Name = "Registration Fee")]
         public short RegistrationFee { get; set; }
-        public byte SubscriptionDuration { get; set; } //0-12 months
+
+        [Display(Name = "Subscription Duration")]
+        public byte SubscriptionDuration { get; set; } //0-200 months
+
+        [Display(Name = "Discount Rate")]
         public byte DiscountRate { get; set; } //0-100%
 
 
