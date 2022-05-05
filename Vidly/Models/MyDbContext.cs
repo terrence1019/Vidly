695a574a82705a3d281587e44c229baa8a2cfeca
Terrence John <terrence1019@outlook.com>
2022-05-05 08:03:17 -0400

    • Updates to Layout and DbContext

--- Vidly/Models/MyDbContext.cs
+++ Vidly/Models/MyDbContext.cs
@@ -9,7 +9,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+
+//This namespace facilitates Data Migrations
 using System.Data.Entity;
+
+
+
+/*
+ * A DbContext instance represents a combination of the Unit Of Work and Repository patterns
+ * such that it can be used to query from a database
+ * and group together changes that will then be written back to the store as a unit.
+ * DbContext is conceptually similar to ObjectContext.
+ */
 
 
 
